9df366c7f9a75e337e4ef05211a8a77f215e19ab
BorakL <lborak88@gmail.com>
2024-01-08 22:33:59 +0100

    minnor changed in api app

--- api/app.js
+++ api/app.js
@@ -12,7 +12,6 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.static('public'))
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 app.use(cors())
