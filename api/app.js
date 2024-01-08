63ab66db0aaa01ba8574fd2564f8d9b1abe6445a
BorakL <lborak88@gmail.com>
2024-01-08 23:11:42 +0100

    uncommented path

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 app.use(cors())
 
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, '../client/build'))); 
 
 app.use("/api/v1/users", userRouter)
