4dfbb563cf138360a9e94f4a789aa0587838ca5b
BorakL <lborak88@gmail.com>
2024-01-09 14:37:01 +0100

    server startup test

--- api/app.js
+++ api/app.js
@@ -18,6 +18,13 @@ app.use(cors())
 
 // app.use(express.static(path.join(__dirname, 'public')))
 
+app.get("/", (req,res,next)=>{
+    res.status(200).json({
+        status: "success",
+        data: {"message":"proba"}
+    })
+})
+
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/topics", topicRouter)
 app.use("/api/v1/conversations", conversationRouter)
